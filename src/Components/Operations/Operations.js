5fe51f45984c7a4dc4d59f179b67f62e92499909
Vadher Dhvanil <VadherDhvanil@JohnDeere.com>
2020-06-26 01:17:35 +0530

    added Submit dummy

--- src/Components/Operations/Operations.js
+++ src/Components/Operations/Operations.js
@@ -4,38 +4,41 @@ import { render } from '@testing-library/react';
 import Select from 'react-select';
 import e from './abc.js';
 
+const defState = {
+    regionOptions: [
+        { value: 0, label: 'Region-one' },
+        { value: 1, label: 'Region-two' }
+    ],
+    lables: [
+        { value: 0, label: 'Alerts' },
+        { value: 1, label: 'Lables' },
+        { value: 2, label: 'Lables-1' }
+    ],
+    keys: [
+        [
+            [{ value: 0, label: 'R-1-l-1-key-1' }, { value: 1, label: 'R-1-l-1-key-2' }, { value: 2, label: 'R-1-l-1-key-3' }],
+            [{ value: 0, label: 'R-1-l-2-key-1' }, { value: 1, label: 'R-1-l-2-key-2' }, { value: 2, label: 'R-1-l-2-key-3' }],
+            [{ value: 0, label: 'R-1-l-3-key-1' }, { value: 1, label: 'R-1-l-3-key-2' }, { value: 2, label: 'R-1-l-3-key-3' }]
+        ],
+        [
+            [{ value: 0, label: 'R-2-l-1-key-1' }, { value: 1, label: 'R-2-l-1-key-2' }, { value: 2, label: 'R-2-l-1-key-3' }],
+            [{ value: 0, label: 'R-2-l-2-key-1' }, { value: 1, label: 'R-2-l-2-key-2' }, { value: 2, label: 'R-2-l-2-key-3' }],
+            [{ value: 0, label: 'R-2-l-3-key-1' }, { value: 1, label: 'R-2-l-3-key-2' }, { value: 2, label: 'R-2-l-3-key-3' }]
+        ]
+
+    ],
+    selectedOptionRegion: '',
+    selectedOptionLable: '',
+    selectedOptionKey: '',
+    selectedOptionValue: '',
+    isChanged: 0
+};
 class Operations extends Component{
 
     constructor(props) {
         super(props);
-        this.state = {
-            regionOptions : [
-                { value: 0, label: 'Region-one' },
-                { value: 1, label: 'Region-two' }
-            ],
-            lables : [
-                { value: 0, label: 'Alerts' },
-                { value: 1, label: 'Lables' },
-                { value: 2, label: 'Lables-1' }
-            ] ,
-            keys :[
-                    [
-                       [ {value: 0,label: 'R-1-l-1-key-1'},{value: 1,label: 'R-1-l-1-key-2'},{value: 2,label: 'R-1-l-1-key-3'}],
-                       [{value: 0,label: 'R-1-l-2-key-1'},{value: 1,label: 'R-1-l-2-key-2'},{value: 2,label: 'R-1-l-2-key-3'}],
-                       [{value: 0,label: 'R-1-l-3-key-1'},{value: 1,label: 'R-1-l-3-key-2'},{value: 2,label: 'R-1-l-3-key-3'}]
-                    ],
-                    [
-                        [{value: 0,label: 'R-2-l-1-key-1'},{value: 1,label: 'R-2-l-1-key-2'},{value: 2,label: 'R-2-l-1-key-3'}],
-                        [{value: 0,label: 'R-2-l-2-key-1'},{value: 1,label: 'R-2-l-2-key-2'},{value: 2,label: 'R-2-l-2-key-3'}],
-                        [{value: 0,label: 'R-2-l-3-key-1'},{value: 1,label: 'R-2-l-3-key-2'},{value: 2,label: 'R-2-l-3-key-3'}]
-                    ]
-
-            ],
-            selectedOptionRegion : '' ,
-            selectedOptionLable :'',
-            selectedOptionKey: '',
-            selectedOptionValue: '',
-        };
+   
+        this.state = defState;
     }
     handleChangeRegion = selectedOptionRegion => {
         this.setState({ selectedOptionRegion:selectedOptionRegion,
@@ -44,17 +47,31 @@ class Operations extends Component{
          });
         console.log(`Region selected:`, selectedOptionRegion);
       };
-    handleChangeLables = selectedOptionLable=> {
+
+    handleChangeLables = selectedOptionLable => {
     this.setState({ selectedOptionLable:selectedOptionLable,
                     selectedOptionKey:''
          });
     console.log(`Lable selected:`, selectedOptionLable);
     };
+
     handleChangeKey = selectedOptionKey => {
         this.setState({ selectedOptionKey: selectedOptionKey});
     console.log(`Key selected:`, selectedOptionKey);
     };
 
+    HandleSubmit = Obj => {
+           //Handle Changes for future Purposes
+        console.log("HANdled SUbmit");
+        this.setState(defState);
+    }
+
+    HandleValueChange = selectabc => {
+        this.setState({
+            selectedOptionKey : selectabc,
+            isChanged: 1
+        })
+    };
 
     render() {
         console.log(e)
@@ -107,10 +124,15 @@ class Operations extends Component{
                     <label>Edit Options :</label>
                         <textarea
                             className={'form-control'}
-                            value= {this.state.selectedOptionKey.value}
+                            value={this.state.selectedOptionKey.value}
+                            onChange={this.HandleValueChange}
                         > </textarea>
-                    <br></br>
-                    <input type="submit" className={'btn btn-success'}></input>
+                        <br></br>
+                        <form onSubmit={this.HandleSubmit}>
+                        <input type="submit" className={'btn btn-success'}
+                            style={{ visibility: this.state.isChanged === 1 ? 'visible' : 'hidden' }}
+                            ></input>
+                            </form>
                 </div> : null
                 }
             </div>
